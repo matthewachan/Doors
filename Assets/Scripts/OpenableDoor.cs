a1ef56949d1aaed96e02974cddf297e320eade3a
Matthew Chan <matthew.a.chan@gmail.com>
2015-10-19 10:36:29 -0700

    Update OpenableDoor.cs

--- Assets/Scripts/OpenableDoor.cs
+++ Assets/Scripts/OpenableDoor.cs
@@ -2,24 +2,26 @@
 using System.Collections;
 
 public class OpenableDoor : MonoBehaviour {
-	public int doorOpenAngle = -90;
-	private bool playerEnter, doorOpen;
-	public float openSpeed = 2;
-	private Vector3 closedRotation, openRotation;
+	// Public variables
 	public GameObject monsterPrefab;
+	public int doorOpenAngle = -90;
+	public float openSpeed = 2;
 	public int monsterRotation = 90;
 	public AudioClip doorCreak1;
 	public AudioClip doorCreak2;
 	public AudioClip monsterGrowl;
+	// Private variables
 	private int counter = 0;
+	private bool playerEnter, doorOpen;
+	private Vector3 closedRotation, openRotation;
 	private AudioSource source;
 	private float volLowRange = .5f;
 	private float volHighRange = 1.0f;
 
 	void Start () {
-		// Obtains the default x, y, z rotational values for a closed door
+		// Obtains the default x, y, z rotational values of the closed door
 		closedRotation = transform.eulerAngles;
-		// Sets the rotation on the door when opening
+		// Sets the x, y, z rotational values for an open door
 		openRotation = new Vector3 (closedRotation.x, closedRotation.y + doorOpenAngle, closedRotation.z);
 		source = GetComponent<AudioSource>(); 
 	}
@@ -29,20 +31,20 @@ public class OpenableDoor : MonoBehaviour {
 		// If the door flag is true, open the door
 		if (doorOpen) {
 			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRotation, Time.deltaTime * openSpeed);
+			// Destroy the monster after the user opens the door
 			Destroy (GameObject.FindGameObjectWithTag("Respawn"), (float).6);
 		} 
 		// If the door flag is false, close the door
 		else {
 			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, closedRotation, Time.deltaTime * openSpeed);
 		}
-		// Toggle door state if user is within the collision box
+		// Player can only open the door when near it
 		if (Input.GetKeyDown (KeyCode.E) && playerEnter) {
 			doorOpen = !doorOpen;
+			// Play a random door creak sound when the door opens
 			source.PlayOneShot (monsterGrowl, volHighRange);
-
 			float vol = Random.Range(volLowRange, volHighRange);
 			int doorSoundSelect = Random.Range(1, 3);
-
 			if (doorSoundSelect == 1)
 				source.PlayOneShot(doorCreak1, vol);
 			else 
@@ -50,29 +52,23 @@ public class OpenableDoor : MonoBehaviour {
 		}
 	}
 	 
-
-	// Toggle entry flag when user within close proximity of the door
+	// Detect when the player is near the door
 	void OnTriggerEnter(Collider player) {
 		if (player.gameObject.tag == "Player")
 			playerEnter = true;
+		// Spawn a monster once when the player is near the door
 		if (counter < 1) {
 			GameObject monster = Instantiate (monsterPrefab, new Vector3 (transform.position.x - (float)1.1, transform.position.y - 2, transform.position.z + 2), Quaternion.Euler (0, monsterRotation, 0)) as GameObject;
 			monster.tag = "Respawn";
 			if (doorOpen)
 				source.PlayOneShot (monsterGrowl, volHighRange);
-
 		}
-
-
 		counter++;
-
 	}
 	
 	void OnTriggerExit(Collider player) {
 		if (player.gameObject.tag == "Player") {
 			playerEnter = false;
-
 		}
 	}
-	
 }
